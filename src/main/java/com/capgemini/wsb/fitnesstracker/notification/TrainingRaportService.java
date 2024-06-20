e235a0c24b940731d08a7fcf054d05f5fd469ad4
Kosamela <dawajzegarek@o2.pl>
2024-06-20 13:26:16 +0200

    email

--- src/main/java/com/capgemini/wsb/fitnesstracker/notification/TrainingRaportService.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/notification/TrainingRaportService.java
@@ -1,10 +1,14 @@
 package com.capgemini.wsb.fitnesstracker.notification;
 
+import com.capgemini.wsb.fitnesstracker.training.api.Training;
 import com.capgemini.wsb.fitnesstracker.training.internal.TrainingRepository;
 import com.capgemini.wsb.fitnesstracker.user.internal.UserRepository;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import com.capgemini.wsb.fitnesstracker.user.api.User;
+import java.util.List;
 
 @Service
 public class TrainingRaportService {
@@ -15,6 +19,20 @@ public class TrainingRaportService {
         this.userRepository = userRepository;
         this.trainingRepository = trainingRepository;
         this.mailSender = mailSender;
+    }
+    private String weekRaport(User user){
+        List<Training> trainingList = trainingRepository.findByUserId(user.getId());
+        Training lastTraining = trainingList.get(0);
+        long trainingCount = trainingList.size();
+        return "Your weekly dose of trainings: " trainingCount+ "\n" +
+                "Your last activity was: " + lastTraining.getActivityType().toString();
+    }
+    private void sendRaport(String email, String raport){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject("Weekly training raport");
+        message.setText(raport);
+        mailSender.send(message);
     }
     @Scheduled(cron = "0 0 0 * * MON")
     public void generateTrainingRaport() {
